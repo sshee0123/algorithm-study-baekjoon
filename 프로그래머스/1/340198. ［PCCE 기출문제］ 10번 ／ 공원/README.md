b2a9a197bc3c87455ca900e3e66ca9f0ebbfd791
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2025-10-15 22:41:38 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 16.73 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/README.md
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 10번 / 공원 - 340198 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340198?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340198) 
 
 ### 성능 요약
 
-메모리: 9.42 MB, 시간: 16.22 ms
+메모리: 9.31 MB, 시간: 16.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 15일 21:53:33
+2025년 10월 15일 22:41:36
 
 ### 문제 설명
 
--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,20 +1,16 @@
-def myfunc(mat,park,r,c):
-    for i in range(r, r+mat):
-        for j in range(c,c+mat):
-            if i<len(park) and j<len(park[0]):
-                if park[i][j] != '-1':
-                    return False
-            else:
-                return False
-    return True
-
-def solution(mats, park):
-    mats.sort(reverse = True) # 크기가 큰 돗자리부터 반복문
+def access(mat, park):
     row = len(park)
     col = len(park[0])
+    for r in range(row-mat+1):
+        for c in range(col-mat+1):
+            # 모든 공간이 -1인 경우 
+            if all(park[i][j] == '-1' for i in range(r,r+mat) for j in range(c,c+mat)):
+                return True
+    return False
+
+def solution(mats, park):
+    mats.sort(reverse = True)
     for mat in mats:
-        for r in range(row-mat+1):
-            for c in range(col-mat+1):
-                if myfunc(mat,park,r,c):
-                    return mat        
+        if access(mat,park):
+            return mat
     return -1
